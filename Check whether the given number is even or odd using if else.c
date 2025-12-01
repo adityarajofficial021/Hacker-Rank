5f7fee3b2dc9df642a5541070ecb0efa9aecaa72
Aditya Raj <adityarajofficial021@gmail.com>
2025-12-01 23:47:31 +0530

    Add files via upload

---
+++ Check whether the given number is even or odd using if else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main(){
+        int num;
+        printf("Enter the number:\n");
+        scanf("%d",&num);
+        
+    if((num%2)==0)
+        {
+            printf("Given number is even");
+        }
+        else{
+            printf("Given number is odd");
+        }
+        return 0;
+    }
