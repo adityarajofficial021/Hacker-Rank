bc20645d119ddc5e24e6e91f0db7e35818001d99
Aditya Raj <adityarajofficial021@gmail.com>
2025-12-03 23:53:08 +0530

    Add files via upload

---
+++ Breaking a 3 Digit Number into Individual Digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+    
+    void split(int n){
+     
+    int A= n/100;
+    int B = (n/10)%10;
+    int C = (n%10);
+    
+    printf("%d %d %d",A,B,C);
+}
+        
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    split(n);
+    
+   
+    return 0;
+}
