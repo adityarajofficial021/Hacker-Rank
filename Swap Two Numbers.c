0b0faf1e3dfd35093e4d6c806c58729764d88d34
Aditya Raj <adityarajofficial021@gmail.com>
2025-12-07 14:49:46 +0530

    Add files via upload

---
+++ Swap Two Numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    
+    
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+   
+    int a = num2;
+    int b = num1;
+    
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    
+    
+    return 0;
+}
