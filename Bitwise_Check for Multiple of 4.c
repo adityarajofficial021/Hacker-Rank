a503689c8b482c6437a50d09e9a94b9515a994ad
Aditya Raj <adityarajofficial021@gmail.com>
2026-01-07 00:15:59 +0530

    Add files via upload

---
+++ Bitwise_Check for Multiple of 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+   int num;
+    scanf("%d", &num);
+    
+    if((num & 3) == 0){
+    printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    
+    return 0;
+}
